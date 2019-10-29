d6b514f5026d814afa0db2caecf5bf05f99cd0e2
Milton Rodrigues Jr <miltonrodrigues.jr@hotmail.com>
2019-10-28 23:25:31 -0300

    Feature: realizado o desenvolvimento até a tarefa: Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '7cef4646c1846a',
+    pass: 'f128f18a07e47d',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
